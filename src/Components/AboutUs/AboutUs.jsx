beb2619c7ab5710e6e96514711913a2a6e413e3f
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-05-03 03:16:29 +0600

    About Us Done

---
+++ src/Components/AboutUs/AboutUs.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div className='my-16 md:w-10/12 w-11/12 mx-auto'>
+                <div className="container mx-auto lg:flex lg:flex-row items-center">
+                    <div className="md:w-1/2">
+                        <img src="https://i.ibb.co/f8LCZ2W/about-1.jpg" alt="About Us Image" className="md:w-10/12 object-cover rounded-2xl shadow-xl shadow-gray-500" />
+                    </div>
+                    <div className="md:w-1/2 ">
+                        <div className="my-8 lg:my-0">
+                            <h2 className='text-orange-500 text-2xl md:text-5xl md:mb-6 mb-4'>About Us</h2>
+                            <p className="text-gray-700 mb-4">
+                            At our Italian restaurant, we strive to bring the best of Italy to your table. Our love for Italian food and culture has inspired us to create a welcoming atmosphere where you can savor the flavors of the Mediterranean. Our chefs use only the freshest and finest ingredients to create authentic Italian dishes that are bursting with flavor. From classic pasta dishes to wood-fired pizzas, our menu has something for everyone. We take pride in our warm and friendly service, ensuring that each guest feels at home from the moment they walk through our doors. Come dine with us and experience a little slice of Italy in the heart of your city.
+                            </p>
+                            <button className="bg-orange-500 text-white font-bold py-2 px-4 rounded hover:bg-orange-700">
+                                Learn More
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+    );
+};
+
+export default AboutUs;
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -8,9 +8,9 @@ const Banner = () => {
   <div className="absolute inset-0 bg-gray-900 bg-opacity-50"></div>
   <div className="absolute inset-0 flex items-center justify-center">
     <div className="text-center">
-      <h1 className="md:w-9/12 mx-auto text-3xl md:text-5xl text-white font-bold leading-tight mb-4 line-">Buon Appetito! Experience Authentic <span className='text-orange-500 italic text-4xl md:text-6xl'>Italian</span> Cuisine </h1>
-      <p className="text-xl text-white mb-8 md:w-8/12 w-10/12 mx-auto">Our chefs use only the freshest ingredients and traditional cooking techniques to create mouthwatering dishes that are sure to satisfy.</p>
-      <div className="flex justify-center">
+      <h1 className="md:w-9/12 mx-auto text-2xl md:text-5xl text-white font-bold leading-tight mb-4 line-">Buon Appetito! Experience Authentic <span className='text-orange-500 italic text-3xl md:text-6xl'>Italian</span> Cuisine </h1>
+      <p className="text-sm md:text-xl text-white mb-8 md:w-8/12 w-10/12 mx-auto">Our chefs use only the freshest ingredients and traditional cooking techniques to create mouthwatering dishes that are sure to satisfy.</p>
+      <div className="flex justify-center md:mb-0 ">
         <button className="border border-white hover:bg-white text-orange-500 font-semibold py-2 px-4 rounded shadow mr-4 duration-300">View Menu</button>
         <button  className="border border-white hover:bg-white text-orange-500 font-semibold py-2 px-4 rounded shadow mr-4 duration-300">Our Sheif</button>
       </div>
